1f4cabb83972dfc9ec2a8533cd62c8eb5b6dc02b
NicolaAstra <mcsnow78@gmail.com>
2019-09-17 16:18:57 +0700

    Updates

---
+++ precache-manifest.82c87ed39fe2d6113afd73a4338c94b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1bea1b2fecab6eb1cdbff5fff7adf10",
+    "url": "/moster-dex/index.html"
+  },
+  {
+    "revision": "578da6c8654acc15399d",
+    "url": "/moster-dex/static/css/main.a2a9bc11.chunk.css"
+  },
+  {
+    "revision": "c6e92b30f6e6948b9ccd",
+    "url": "/moster-dex/static/js/2.cf7af900.chunk.js"
+  },
+  {
+    "revision": "578da6c8654acc15399d",
+    "url": "/moster-dex/static/js/main.008b7756.chunk.js"
+  },
+  {
+    "revision": "ed150d0ef3d4efb2ccac",
+    "url": "/moster-dex/static/js/runtime~main.20946b09.js"
+  }
+]);
